65ca58c42c759ff4f7af632f4399ddefd0c015c7
Ravi Kowlessur <ravi.kowlessur@gmail.com>
2019-06-17 16:25:34 +0400

    Bootstraping Categories

--- src/main/java/guru/springframework/springrestclientexamples/services/ApiServiceImpl.java
+++ src/main/java/guru/springframework/springrestclientexamples/services/ApiServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import guru.springframework.api.domain.User;
 import guru.springframework.api.domain.UserData;
@@ -19,7 +20,13 @@ public class ApiServiceImpl implements ApiService{
 	
 	@Override
 	public List<User> getUsers(Integer limit) {
-		UserData userData = restTemplate.getForObject("http://apifaketory.com/api/user?limit="+limit,UserData.class);
+		
+		//UserData userData = restTemplate.getForObject("?limit="+limit,UserData.class);
+
+		String apiUri = "http://apifaketory.com/api/user";
+		
+		UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromUriString(apiUri).queryParam("limit", limit);
+		UserData userData = restTemplate.getForObject(uriBuilder.toUriString(), UserData.class);		
 		return userData.getData();
 	}
 
